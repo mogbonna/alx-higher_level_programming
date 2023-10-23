023fdac7ce8bd3f781df4e1ec6f74a7d68ea1a69
mogbonna <mogbonna045@gmail.com>
2023-10-23 05:34:06 -0700

    The first set of questions done for now

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        print("")
+    except IndexError:
+        print("")
+    return (printed)
