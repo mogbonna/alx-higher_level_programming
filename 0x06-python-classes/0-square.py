39decffe1583e7357ac2957a19c88fd4bc63bede
mogbonna <mogbonna045@gmail.com>
2023-10-24 05:40:02 -0700

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of the Square class.
+"""
+
+class Square:
+    """
+    The Square class represents a square.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is the readme for 0x06-python-classes projects
