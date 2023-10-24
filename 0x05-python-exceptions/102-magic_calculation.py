a1904967352ce96a3c930cd6918cb91560b26d74
mogbonna <mogbonna045@gmail.com>
2023-10-23 23:40:21 -0700

    learning daily

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too Far')
             result += a ** b / i
-        except:
+        except Exception:
             result = b + a
             break
     return (result)
