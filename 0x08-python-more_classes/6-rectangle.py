beaa146430d1e507f71eee99d30bcccdca0ca0f4
mogbonna <mogbonna045@gmail.com>
2023-10-30 10:07:55 -0700

    Detect instance deletion

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""
+This class defines a rectangle
+"""
+
+
+class Rectangle:
+    """Class that defines a Rectangle
+    Attributes:
+        width: width of Rectangle
+        height: height of rectangle
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initializer with default width and height equal 0"""
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
+        type(self).number_of_instances += 1
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @property
+    def width(self):
+        """property to retrieve private instance width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """to set private instance width"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """property to retrieve private instance height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """to set private instance height"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """public instance method that returns the rectangle area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """public instance method that returns the rectangle perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return (2 * self.__width + 2 * self.__height)
+
+    def __str__(self):
+        """str instance method that returns the rectangule to print with # """
+        new_str = ""
+        if self.__width == 0 or self.__height == 0:
+            return (new_str)
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    new_str += '#'
+                if i in range(self.__height - 1):
+                    new_str += '\n'
+            return (new_str)
+
+    def __repr__(self):
+        """repr instance method returns string representation of rectangule """
+        return 'Rectangle(%s, %s)' % (self.__width, self.__height)
