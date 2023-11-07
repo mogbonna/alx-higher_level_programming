3e39077e1c2e4815d046b5491922d2918ff38727
mogbonna <mogbonna045@gmail.com>
2023-11-07 10:18:47 -0800

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Pascal Triangle Module"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of Pascal's triangle.
+    
+    Args:
+    n (int): The number of rows the Pascal's triangle should have.
+    
+    Returns:
+    List[List[int]]: A list of lists, of the Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            # Add the number above and to the left with the number above and to the right.
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
