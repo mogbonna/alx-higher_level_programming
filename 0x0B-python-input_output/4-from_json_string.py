531c1bc0c34ebfffad37886ccd19905f8865c0d2
mogbonna <mogbonna045@gmail.com>
2023-11-07 05:41:05 -0800

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -11,7 +11,4 @@ def from_json_string(my_str):
     Returns:
     The Python data structure.
     """
-    
-    # The json module is used for deserialization from JSON.
-    # The loads method deserializes my_str to a Python object.
     return json.loads(my_str)
