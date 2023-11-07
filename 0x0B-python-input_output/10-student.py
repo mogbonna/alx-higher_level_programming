a0ad98d119801816d1f6c501d3ecf046f8bf412f
mogbonna <mogbonna045@gmail.com>
2023-11-07 09:59:45 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -20,4 +20,7 @@ class Student:
         Returns:
         A dictionary containing key/value pairs of attributes.
         """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
