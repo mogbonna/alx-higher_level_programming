114e67fbe972141f47289a560863bb187d736f77
mogbonna <mogbonna045@gmail.com>
2023-11-07 05:06:55 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This returns the number of characters written."""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file using UTF-8 encoding and returns the number of characters written.
+
+    Args:
+    filename (str): The path to the file to be written to.
+    text (str): The string to write to the file.
+
+    Returns:
+    int: The number of characters written to the file.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        char_written = f.write(text)
+        return char_written
