a25f56c9621a1a69c1aaf81ecb2168f01f3d0922
mogbonna <mogbonna045@gmail.com>
2023-11-13 18:52:45 -0800

    Doing Hard Things is Hard

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Square class Module."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A Square class."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class Constructor."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns Square string information."""
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Size of this square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """Updates instance attributes using internal method via */**args."""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes via args & kwargs."""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """Returns dictionary representation"""
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
