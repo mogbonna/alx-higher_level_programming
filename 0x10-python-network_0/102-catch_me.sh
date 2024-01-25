657fb61233f9cfcc844e97b502a1a31755ff8a77
mogbonna <mogbonna045@gmail.com>
2024-01-25 12:47:06 -0800

    Doing Hard Things is Hard

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Use curl to make a POST request to the specified URL
-curl -s -X POST -d "user_id=98" http://0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
